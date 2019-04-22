ca9c80b04a6419b117476e3cfaa31978dacce8a7
unknown <you@example.com>
2019-04-22 13:26:13 +0200

    Excersize2

--- src/optional/BeerOnTheWall.java
+++ src/optional/BeerOnTheWall.java
@@ -8,6 +8,17 @@ package optional;
 
 public class BeerOnTheWall {
 	public static void main(String[] args) {
+		
+		for (int i=99; i>1; i--)
+		{
+			System.out.println(i + " bottles of beer on the wall, "+ i + " bottles of beer. \n"+ 
+			"Take one down and pass it around "+ (i-1) + " bottles of beer on the wall.\n");
+		}
+			System.out.println("1 bottle of beer on the wall," + " 1 bottle of beer. \n"+
+			"Take one down and pass it around, no more bottles of beer on the wall.");
+			
+			System.out.println("\nNo more bottles of beer on the wall, no more bottles of beer.\n" +
+			"Go to the store and buy some more, 99 bottles of beer on the wall.");
 
 	}
 }
